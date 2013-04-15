02298e5603e48713da430a15a011f0c662addc5f
Stephen Moock <stephen.moock@moovweb.com>
2013-04-14 21:45:27 -0700

    adding files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'dad298ea7ffeae4e4c3f035d6f59c2091257964c258151e739788f2d8c68ff4c76e5bc7124c269a50239463c308f57cdc410e954599d5b6094eccea27c5a3740'
